793eef2a6a5b1f5e445e97407c972885e4c409c1
Jeremy Lincoln <bossco28@gmail.com>
2018-01-04 09:19:34 -0500

    Update public folder

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+//middleware 
+app.use(express.static(__dirname + '/../public'))
+
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
